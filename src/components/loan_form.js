aac1b04c55dc267f3280eb03380cccaf6a94603d
Umeshpotha <umeshpotha123@gmail.com>
2024-05-12 03:05:59 +0530

    Change U

--- src/components/loan_form.js
+++ src/components/loan_form.js
@@ -1,46 +1,97 @@
-import React from 'react';
-import './loan_form.css';
+import React from "react";
+import "./loan_form.css";
 
 function LoanApplicationForm() {
-    return (
-        <div>
-            <h2>Loan Application Form</h2>
-            <form action="#" method="post" encType="multipart/form-data">
-                <label htmlFor="Name">Name:</label>
-                <input type="text" id="Name" name="Name" placeholder="Enter the name" required /><br /><br />
-                <label htmlFor="dob">Date of Birth:</label>
-                <input type="date" id="dob" name="dob" required /><br /><br />
-                <label htmlFor="gender">Gender:</label>
-                <select id="gender" name="gender" required>
-                    <option value="">Select</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                </select><br /><br />
-                <label htmlFor="marital_status">Marital Status:</label>
-                <select id="marital_status" name="marital_status" required>
-                    <option value="">Select</option>
-                    <option value="single">Single</option>
-                    <option value="married">Married</option>
-                    <option value="divorced">Divorced</option>
-                    <option value="widowed">Widowed</option>
-                </select><br /><br />
-                <label htmlFor="dependents">Number of Dependents:</label>
-                <input type="number" id="dependents" name="dependents" required /><br /><br />
-                <label htmlFor="education">Education:</label>
-                <input type="text" id="education" name="education" required /><br /><br />
-                <label htmlFor="self_employed">Self Employed:</label>
-                <input type="checkbox" id="self_employed" name="self_employed" /><br /><br />
-                <label htmlFor="income">Income:</label>
-                <input type="number" id="income" name="income" required /><br /><br />
-                <label htmlFor="co_income">Co-Applicant Income:</label>
-                <input type="number" id="co_income" name="co_income" /><br /><br />
-                <label htmlFor="loan_amount">Loan Amount:</label>
-                <input type="number" id="loan_amount" name="loan_amount" required /><br /><br />
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Loan Application Form</h2>
+      <form action="#" method="post" encType="multipart/form-data">
+        <label htmlFor="Name">Name:</label>
+        <input
+          type="text"
+          id="Name"
+          name="Name"
+          placeholder="Enter the name"
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="dob">Date of Birth:</label>
+        <input type="date" id="dob" name="dob" required />
+        <br />
+        <br />
+        <label for="Gender">Gender:</label>
+        <select id="Gender" name="Gender" required>
+          <option value="">Select</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <br />
+        <br />
+        <label for="Married">Married:</label>
+        <select id="Married" name="Married" required>
+          <option value="Yes">Yes</option>
+          <option value="No">No</option>
+        </select>
+        <br />
+        <br />
+        <label for="Dependents">Dependents:</label>
+        <select id="Dependents" name="Dependents" required>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <br />
+        <br />
+        <label for="Education">Education:</label>
+        <select name="Education" id="Education">
+          <option value="Graduate">Graduate</option>
+          <option value="Not Graduate">Not Graduate</option>
+        </select>
+        <br />
+        <br />
+        <label for="Self_Employed">Self Employed:</label>
+        <select name="Self_Employed" id="Self_Employed">
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select>
+
+        <br />
+        <br />
+        <label for="ApplicantIncome">Income:</label>
+        <input
+          type="number"
+          id="ApplicantIncome"
+          name="ApplicantIncome"
+          required
+        />
+        <br />
+        <br />
+        <label for="CoapplicantIncome">Co-Applicant Income:</label>
+        <input type="number" id="CoapplicantIncome" name="CoapplicantIncome" />
+        <br />
+        <br />
+        <label for="LoanAmount">Loan Amount:</label>
+        <input type="number" id="LoanAmount" name="LoanAmount" required />
+        <label for="Loan_Amount_Term">Term (Days):</label>
+        <input
+          type="number"
+          id="Loan_Amount_Term"
+          name="Loan_Amount_Term"
+          required
+        />
+        <label for="" id="Property_Area">
+          Property_Area
+        </label>
+        <select name="Property_Area" id="Property_Area">
+          <option value="Rural">Rural</option>
+          <option value="Urban">Urban</option>
+          <option value="Semiurban">Semi Urban</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default LoanApplicationForm;
